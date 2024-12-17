fcfeed0b5d8029617cb0101369c6bbb0fdcbf6c8
Jens Blawatt <jens.blawatt@codecentric.de>
2024-12-17 07:29:13 +0100

    add vim-test

--- lua/plugins/simple.lua
+++ lua/plugins/simple.lua
@@ -68,4 +68,14 @@ return {
   --     },
   --   },
   { "s1n7ax/nvim-window-picker" },
+  {
+    "vim-test/vim-test",
+    config = function()
+      vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
+      vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+      vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+      vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+      vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
+    end,
+  },
 }
