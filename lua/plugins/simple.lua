0d2678c95af6ebb6c0e30d8a8a9a70c5984eef08
Jens Blawatt <jens.blawatt@codecentric.de>
2024-12-19 22:12:53 +0100

    config + blink config

--- lua/plugins/simple.lua
+++ lua/plugins/simple.lua
@@ -78,4 +78,12 @@ return {
       vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
     end,
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require("tiny-inline-diagnostic").setup()
+    end,
+  },
 }
